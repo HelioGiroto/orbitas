e646cc2be3c3097c9436b5476cccb206f2b1825a
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2017-08-24 23:21:14 -0300

    Update 3-compara_localizacoes.sh

--- 3-compara_localizacoes.sh
+++ 3-compara_localizacoes.sh
@@ -1,59 +1,59 @@
 #!/bin/bash
 
-# Compara as posições de Jupiter com Lua 
+				# Faz o cruzamento das informações processadas até aqui...
 
-
-# cat Jupiter_em_Virgo.dat
-
-#for linha in $(awk '{print $0}' Jupiter_em_Virgo.dat)
-#do
-#	awk -v linha="$linha" '{ if($1 == linha) print $0}' Lua_em_Virgo.dat >> coincide_awk.txt	
-
-#done
-
+# Compara as posições de Jupiter com Lua :
 
 for linha in $(cat Jupiter_em_Virgo.dat)
 do
 	grep "$linha" Lua_em_Virgo.dat >> JL_grep.dat 2>/dev/null 
-
 done
 
-cat JL_grep.dat | sort -nru > JL.dat
+cat JL_grep.dat | sort -nru > JL.dat				# Cada CAT/SORT ordena os dados (Por data crescente) do arq gerado.
 
+
+# Compara Jupiter e Lua com Sol:
 
 for linha in $(cat JL.dat)
 do
 	grep "$linha" Sol_em_Virgo.dat >> JLS_grep.dat 2>/dev/null 
-
 done
 
-cat JLS_grep.dat | sort -nru > JLS.dat
+cat JLS_grep.dat | sort -nru > JLS.dat				# CAT/SORT
+
+
+# Compara Jupiter+Lua+Sol com Marte:
 
 for linha in $(cat JLS.dat)
 do
 	grep "$linha" Marte_em_Leo.dat >> JLSM_grep.dat 2>/dev/null
 done
 
-cat JLSM_grep.dat | sort -nru > JLSM.dat
+cat JLSM_grep.dat | sort -nru > JLSM.dat			# CAT/SORT
 
+
+# Compara Jupiter+Lua+Sol+Marte com Venus:
 
 for linha in $(cat JLSM.dat)
 do
 	grep "$linha" Venus_em_Leo.dat >> JLSMV_grep.dat 2>/dev/null
 done
 
-cat JLSMV_grep.dat | sort -nru > JLSMV.dat
+cat JLSMV_grep.dat | sort -nru > JLSMV.dat			# CAT/SORT
 
+
+# Compara Jupiter+Lua+Sol+Marte+Venus com Mercurio:
 
 for linha in $(cat JLSMV.dat)
 do
 	grep "$linha" Mercurio_em_Leo.dat >> JLSMVMc_grep.dat 2>/dev/null
 done
 
-cat JLSMVMc_grep.dat | sort -nru > JLSMVMc.dat
+cat JLSMVMc_grep.dat | sort -nru > JLSMVMc.dat			# CAT/SORT
 
 
+rm *_grep.dat			# "Limpa" (deleta) arquivos des-ordeados....
 
-
-
-
+echo "Finalizado!"
+												# Autor: Helio Giroto
+												
